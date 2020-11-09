874bce641104d25ead9cf7af2ed67ff2b57dbaaa
rocastro300 <71615855+rocastro300@users.noreply.github.com>
2020-11-09 01:21:49 -0600

    Add files via upload

---
+++ colormixer/code.js
@@ -0,0 +1,9 @@
+function colorMixer()
+{
+    var red = document.getElementById("red").value;
+    var green = document.getElementById("green").value;
+    var blue = document.getElementById("blue").value;
+
+    document.getElementById("sample").style = "background-color: rgb(" + red + "," + green + "," + blue + ");";
+    document.getElementById("color-output").innerHTML = "background-color: rgb(" + red + "," + green + "," + blue + ");";
+}
